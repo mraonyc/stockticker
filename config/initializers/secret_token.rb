917f9339ffd10bebba639e9388b64d491debd135
Maya Rao <rao.maya@gmail.com>
2013-02-04 22:48:53 -0500

    create app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stockticker::Application.config.secret_token = 'ea921997988e86940e949cd2d05c721d54ec506362f4fdf4e42a7f3d9a70f9636d3f4540db34bd632307c5b90b81a633f3568eb6c0bd0c067a34d670b0ed1c9f'
